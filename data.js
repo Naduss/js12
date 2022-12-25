157a312dac350b73a22521f5b2e5bcba9755cedb
Nadya <nadus@ngs.ru>
2022-12-25 20:26:48 +0700

    data.js добавили

---
+++ data.js
@@ -0,0 +1,34 @@
+const offer_content = `{ 
+    ["cart_items":
+    "products_idOne":{
+        "productName": "dressOne",
+        "prouctInform":"forMan",
+        "productPrice": 52
+    } 
+    "products_idTwo":{
+        "productName":"dressTwo",
+        "prouctInform":"forWomen",
+        "productPrice":52
+    }
+    "products_idThree":{
+        "productName":"dressThree",
+        "prouctInform":"forMan",
+        "productPrice":52
+    }
+    "products_idFour":{
+        "productName":"dressFour",
+        "prouctInform":"forMan",
+        "productPrice":52
+    }
+    "products_idFive":{
+        "productName":"dressFive",
+        "prouctInform":"forWomen",
+        "productPrice":52
+    }
+    "products_idSix":{
+        "productName":"dressSix",
+        "prouctInform":"forWomen",
+        "productPrice":52
+    }
+]
+}`
